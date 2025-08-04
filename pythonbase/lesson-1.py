b30d20724099f245b706932e8d0508c952108b22
1guy <ginioness@gmail.com>
2025-08-04 20:06:07 +0200

    Типы данных

--- pythonbase/lesson-1.py
+++ pythonbase/lesson-1.py
@@ -1,1 +1,37 @@
-print('hello')
+num = 15
+print(id(num)) # id() - выводим номер переменной в оперативной памяти
+
+# Целое число / integer / int
+my_int = 10
+print(my_int, type(my_int))
+
+# Дробные числа / вещественные числа / числа с плавающей строчкой / float
+my_float = 0.93
+print(my_float, type(my_float))
+
+# Строки / string / str
+my_str_1 = 'bab"ulya'
+my_str_2 = "babu'lya"
+print(my_str_1, type(my_str_1))
+print(my_str_2, type(my_str_2))
+
+# Списки / list
+my_list = [23, 29, 'che', 0,2]
+print(my_list, type(my_list))
+
+# Кортеж / tuple
+my_tuple = ['Kiselev', 'Alexei', '24']
+print(my_tuple, type(my_tuple))
+
+# Множество / set
+my_set = {1, 2, 3, 2, 2 ,2, 3}
+print(my_set, type(my_set))
+
+# Словарь / dictionary / dict
+my_dict = {'name':'Larisa', 'age':91, 'lastname':'Nesterova'}
+print(my_dict, type(my_dict))
+
+# Логический тип данных / boolean / bool
+my_bool1 = True
+my_bool2 = False
+print(my_bool1, type(my_bool1))
