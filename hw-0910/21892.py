c2489c630d0cf884339a642c059c52f2c24f7e31
admin <ginioness@gmail.com>
2025-11-09 13:52:33 +0200

    https://kompege.ru/variant?kim=25123353

---
+++ hw-0910/21892.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+screensize(3000, 3000)
+tracer(0)
+m = 30
+
+rt(90)
+
+for i in range(7):
+    rt(45)
+    fd(11*m)
+    rt(45)
+
+up()
+for x in range(-15,10):
+    for y in range(-10,10):
+        goto(x*m,y*m)
+        dot(3,"red")
+
+update()
+done()
+
+# 113
