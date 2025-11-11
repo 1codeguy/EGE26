529a76f4e3921fe92d25a20dd99e43a067d8d909
admin <ginioness@gmail.com>
2025-11-11 23:53:09 +0200

    hard hw

---
+++ hw-11-11/12721.py
@@ -0,0 +1,29 @@
+def convert(num, sys):
+    res = ''
+    while num != 0:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+
+for n in range(1, 100_000):
+    k = 0
+    r = convert(n, 8)
+    while r != 0:
+        if int(r[-1:], 8) % 2 != 0:
+            k += 1
+            l = int(r, 8)
+            int(r, 8) //= 8
+    if k % 2 == 0:
+        r = r + '46'
+    else:
+        r = convert((r % 8) * 5, 8) + r
+    r = int(r, 8)
+    if n >= 80:
+        ans.append(r)
+
+print(min(ans))
+
+
+
---
+++ hw-11-11/22270.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def convert(num, sys):
+    res = ''
+    while num != 0:
+        res += str(num % sys)
+        num = num // sys
+    return res[::-1]
+
+for n in range(1, 100_000):
+    r = convert(n, 4)
+    if int(r[:1]) == 3:
