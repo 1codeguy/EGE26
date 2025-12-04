d01ce0f2ef53f7319f2a019778794c51f63fe69c
1guy <ginioness@gmail.com>
2025-12-04 20:07:04 +0200

    reduct

--- homework/hw-03-12/22269.py
+++ homework/hw-03-12/22269.py
@@ -10,8 +10,8 @@ def convert(num, sys):
 ans = []
 for N in range(1, 100_000):
     R = convert(N, 5)
-    new_R = 0
-    if R[-1] == 0:
+    new_R = ''
+    if R[-1] == '0':
         for i in R:
             if i == '1':
                 new_R += '4'
@@ -24,7 +24,13 @@ for N in range(1, 100_000):
     else:
         R = '3' + R[1:] + '42'
     R = int(R, 5)
-    if R < 1992:
+    if R < 1922:
         ans.append([R, N])
-print(max(ans))
 
+all_n = []
+max_R = max(ans)[0]
+for i in ans:
+    if max_R == i[0]:
+        all_n.append(i[1])
+
+print(min(all_n))
--- homework/hw-03-12/8472.py
+++ homework/hw-03-12/8472.py
@@ -1,6 +1,16 @@
-from string import ascii_letters as ian
-for x in ian[:100]:
-    num1 = 7*100**7 + a*100**6 + x*100**5 + 0*100**4 + 0*100**3 + 1*100**2 + 2*100 + 1
-    num2 = f'ba64{x}'
-    num3 = f'{x}98012c'
-print(num1)
+from string import printable as sad
+
+def convert(num, sys):
+    res = ''
+    while num:
+        res += sad[num % sys]
+        num //= sys
+    return res[::-1]
+
+for x in range(1, 100):
+    num1 = 7*100**7 + 10*100**6 + x*100**5 + 0*100**4 + 0*100**3 + 1*100**2 + 2*100*1 + 3*100*0
+    num2 = 1*100**5 + 11*100**4 + 10*100**3 + 6*100**2 + 4*100**1 + x*100**0
+    num3 = x*100**6 + 9*100**5 + 8*100**4 + 0*100**3 + 1*100**2 + 2*100**1 + 12*100**0
+    num = num1 - num2 + num3
+    if num % 21 == 0:
+        print(convert(x, 6))
