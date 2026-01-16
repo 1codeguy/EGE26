091be1952921f107e87438e8fe01df055340aede
admin <ginioness@gmail.com>
2026-01-16 23:28:29 +0200

    отработка n2 и n5

---
+++ Task-15/25354.py
@@ -0,0 +1,11 @@
+def f(x):
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            F = (78125 != (y + 4*x)) or (A > x) and (A > y)
+            if not F:
+                return False
+    return True
+
+for A in range(1, 100_000):
+    if f(A):
+        print(A)
---
+++ Task-2/25341.py
@@ -0,0 +1,15 @@
+from itertools import product, permutations
+
+def f(x, y, z, w):
+    return (w == z) or (not (y <= w)) or (not x)
+
+for i in product((0, 1), repeat=5):
+    table = [
+        (i[0], 0, 1, 0),
+        (i[1], 1, 1, i[2]),
+        (0, i[3], i[4], 0)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            if [f(**dict(zip(p, t))) for t in table] == [0, 0, 0]:
+                print(*p, sep='')
