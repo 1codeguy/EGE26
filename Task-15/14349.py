d9d492304d376d9c401980ced2ee46b08fba0c16
1guy <ginioness@gmail.com>
2026-01-22 19:50:24 +0200

    hardwork

---
+++ Task-15/14349.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def f(x):
+    B = 54 <= x <= 120
+    C = 78 <= x <= 151
+    A = A1 <= x <= A2
+    return C <= ((B and (not A)) <= (not C))
+
+lineA = [54, 78, 120, 151]
+lineX = [60, 100, 130]
+
+ans = []
+for A1, A2 in combinations(lineA, 2):
+    if all(f(x) for x in lineX):
+        ans.append(A2 - A1)
+
+print(min(ans))
