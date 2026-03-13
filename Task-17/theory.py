2d55c1e2fad46b87d2f4a83af7ffd8655ba24838
1guy <ginioness@gmail.com>
2026-03-13 17:56:28 +0200

    Zadania 17ie

---
+++ Task-17/theory.py
@@ -0,0 +1,7 @@
+with open('путь до файла') as file:
+    # Считывает весь файл. Возвращает str
+    data = file.read()
+    # Считывает одну строку до символа \n. Возвращает str
+    data = file.readline()
+    # Считывает все строки. Возвращает list[str]
+    data = file.readlines()
