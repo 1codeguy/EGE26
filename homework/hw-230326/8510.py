f8547725557ae4b65626151c63bb56b83e3f8815
admin <ginioness@gmail.com>
2026-03-24 23:51:59 +0200

    hw

--- homework/hw-230326/8510.py
+++ homework/hw-230326/8510.py
@@ -1,18 +1,12 @@
 with open(r'/Users/admin/Desktop/N24/24_8510.txt') as file:
     data = file.readline()
 
-ans = 0
-for i in range(len(data) - 1):
-    if data[i:i + 1] != 'NN' and \
-            data[i:i + 1] != 'OO' and \
-            data[i:i + 1] != 'PP':
-        for j in range(i + 1, len(data) - 1):
-            if data[j:j + 1] != 'NN' and \
-                    data[j:j + 1] != 'OO' and \
-                    data[j:j + 1] != 'PP':
-                continue
-            else:
-                ans = max(ans, len(data[i:j + 1]))
-                break
+for i in 'NOP':
+    data = data.replace(i, '&')
 
-print(ans)
+while '&&' in data:
+    data = data.replace('&&', '& &')
+
+data = data.split()
+
+print(len(max(data, key=len)))
