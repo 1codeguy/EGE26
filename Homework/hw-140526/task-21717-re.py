fd97c466613ac3105de722fc415a006afac46be3
1guy <ginioness@gmail.com>
2026-05-22 15:55:31 +0200

    classwork

--- Homework/hw-140526/task-21717-re.py
+++ Homework/hw-140526/task-21717-re.py
@@ -15,4 +15,22 @@ for i in range(len(starts) - 129):
     ans = min(ans, len_line)
 
 print(ans)
-# using ai
+
+##############################################################
+
+with open(r'..\files\24_21717.txt') as file:
+    data = file.readline()
+
+data = data.replace('RSQ', 'Rsq rsQ')
+data = data.split()
+
+ans = 10 ** 10
+for i in range(len(data) - 128 - 1):
+    line = ''.join(data[i:i + 129]).replace('sqrs', 'S')
+    cnt = 0
+    for j in data[i + 129][3:]:
+        cnt += 1
+        if j not in 'Qq':
+            break
+    ans = min(ans, len(line) + cnt)
+print(ans)
